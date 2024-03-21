580d691c5c87cd7e0f34da6b53264ffe4b09b87a
Nikita <malykhnik@mail.ru>
2024-03-21 15:06:51 +0400

    Return controller objects changed to ResponseEntity

--- BulletinBoard/src/main/java/com/malykhnik/bulletinboard/controller/TopicController.java
+++ BulletinBoard/src/main/java/com/malykhnik/bulletinboard/controller/TopicController.java
@@ -8,6 +8,8 @@ import com.malykhnik.bulletinboard.exception.*;
 import com.malykhnik.bulletinboard.service.TopicService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,50 +21,53 @@ public class TopicController {
     private final TopicService topicService;
 
     @GetMapping("/getAllTopics")
-    public List<Topic> getAllTopics(@RequestParam(defaultValue = "1") int page,
-                                    @RequestParam(defaultValue = "3") int pageSize) throws IllegalPageSizeException {
-        return topicService.getAllTopics(page, pageSize);
+    public ResponseEntity<List<Topic>> getAllTopics(@RequestParam(defaultValue = "1") int page,
+                                                    @RequestParam(defaultValue = "3") int pageSize) throws IllegalPageSizeException {
+        return ResponseEntity.ok(topicService.getAllTopics(page, pageSize));
     }
 
     @GetMapping("/getMessagesInTopic/{id}")
-    public List<Message> getMessageInTopicById(@PathVariable int id,
-                                               @RequestParam(defaultValue = "1") int page,
-                                               @RequestParam(defaultValue = "3") int pageSize) throws TopicNotFound, IllegalPageSizeException {
-        return topicService.getMessagesInTopicById(id, page, pageSize);
+    public ResponseEntity<List<Message>> getMessageInTopicById(@PathVariable int id,
+                                                               @RequestParam(defaultValue = "1") int page,
+                                                               @RequestParam(defaultValue = "3") int pageSize) throws TopicNotFound, IllegalPageSizeException {
+        return ResponseEntity.ok(topicService.getMessagesInTopicById(id, page, pageSize));
     }
 
     @PostMapping("/createTopic")
-    public void createTopic(@Valid @RequestBody Topic topic) {
+    public ResponseEntity<Void> createTopic(@Valid @RequestBody Topic topic) {
         topicService.addTopic(topic);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/createMessageInTopic/{id}")
-    public void createMessage(@Valid @RequestBody Message message,
-                                      @PathVariable int id) throws TopicNotFound {
+    public ResponseEntity<Void> createMessage(@Valid @RequestBody Message message,
+                                              @PathVariable int id) throws TopicNotFound {
         topicService.addMessage(message, id);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/updateMessage/{topicId}/{mesId}")
-    public Message updateMessage(@Valid @RequestBody MessageDto messageDto,
-                                @PathVariable int topicId,
-                                 @PathVariable int mesId) throws UserNotAuthenticated, NoUserPermissions, TopicNotFound, MessageNotFound {
-        return topicService.updateMessage(messageDto, topicId, mesId);
+    public ResponseEntity<Message> updateMessage(@Valid @RequestBody MessageDto messageDto,
+                                                 @PathVariable int topicId,
+                                                 @PathVariable int mesId) throws UserNotAuthenticated, NoUserPermissions, TopicNotFound, MessageNotFound {
+        return ResponseEntity.ok(topicService.updateMessage(messageDto, topicId, mesId));
     }
 
     @PutMapping("/updateTopic/{topicId}")
-    public Topic updateTopic(@RequestBody TopicDto topicDto,
-                             @PathVariable int topicId) throws TopicNotFound {
-        return topicService.updateTopic(topicDto, topicId);
+    public ResponseEntity<Topic> updateTopic(@RequestBody TopicDto topicDto,
+                                             @PathVariable int topicId) throws TopicNotFound {
+        return ResponseEntity.ok(topicService.updateTopic(topicDto, topicId));
     }
 
     @DeleteMapping("/deleteMessage/{topicId}/{mesId}")
-    public Message deleteMessage(@PathVariable int topicId,
-                              @PathVariable int mesId) throws UserNotAuthenticated, NoUserPermissions, TopicNotFound, MessageNotFound {
-        return topicService.deleteMessage(topicId, mesId);
+    public ResponseEntity<Message> deleteMessage(@PathVariable int topicId,
+                                                 @PathVariable int mesId) throws UserNotAuthenticated, NoUserPermissions, TopicNotFound, MessageNotFound {
+        return ResponseEntity.ok(topicService.deleteMessage(topicId, mesId));
     }
 
     @DeleteMapping("/deleteTopic/{topicId}")
-    public Topic deleteTopic(@PathVariable int topicId) throws TopicNotFound {
-        return topicService.deleteTopic(topicId);
+    public ResponseEntity<Topic> deleteTopic(@PathVariable int topicId) throws TopicNotFound {
+        return ResponseEntity.ok(topicService.deleteTopic(topicId));
     }
 }
+
